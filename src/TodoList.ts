52de0625123678c0bff64fe367d875623532a9fc
alinepmarcondes <aline.patta@ges.inatel.br>
2024-03-15 23:32:11 -0300

    :sparkles: feat: validate update and remove errors

--- src/TodoList.ts
+++ src/TodoList.ts
@@ -1,50 +1,62 @@
 export type Task = {
-    title: string,
-    description: string,
-    targetDate: string,
-    type?: string,
-    priority?: string,
-    subTasks?: Task []
-}
-
-export type UpdateTask = {
-  title?: string,
-  description?: string,
-  targetDate?: string,
+  title: string,
+  description: string,
+  targetDate: string,
   type?: string,
   priority?: string,
   subTasks?: Task []
 }
 
+export type UpdateTask = {
+title?: string,
+description?: string,
+targetDate?: string,
+type?: string,
+priority?: string,
+subTasks?: Task []
+}
+
 export class ToDoList {
-  private tasks: Task[] = []
+private tasks: Task[] = []
 
-  add (task: Task) {
-    const missingProperties = ['title', 'description', 'targetDate'].filter(
-      (prop) => !Object.keys(task).includes(prop)
-    )
-    try {
-      if (missingProperties.length > 0) {
-        return 'Missing properties in task object'
-      }
-      this.tasks.push(task)
-    } catch (error) {
-      return error
+add (task: Task) {
+  const missingProperties = ['title', 'description', 'targetDate'].filter(
+    (prop) => !Object.keys(task).includes(prop)
+  )
+  try {
+    if (missingProperties.length > 0) {
+      return 'Missing properties in task object'
     }
+    this.tasks.push(task)
+  } catch (error) {
+    return error
+  }
+}
+
+getTasks () {
+  return this.tasks
+}
+
+updateTask(index: number, task: UpdateTask) {
+  const existingTask = this.tasks[index];
+
+  if (
+      JSON.stringify(existingTask) === JSON.stringify({ ...existingTask, ...task })
+  ) {
+      throw new Error('Cannot update task with the same parameters.');
   }
 
-  getTasks () {
-    return this.tasks
-  }
-
-  updateTask (index: number, task: UpdateTask) {
-    this.tasks[index] = {
-      ...this.tasks[index],
+  this.tasks[index] = {
+      ...existingTask,
       ...task
-    }
+  };
+}
+
+removeTask(index: number) {
+  if (index < 0 || index >= this.tasks.length) {
+      throw new Error('Index is out of bounds. Cannot remove task.');
   }
 
-  removeTask (index: number) {
-    this.tasks.splice(index, 1)
-  }
+  this.tasks.splice(index, 1);
+}
 }
